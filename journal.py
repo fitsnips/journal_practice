a1e8da405e5929ff29c070655161eb85f739da71
Joshua Miller <joshua.miller@rd.io>
2019-02-16 20:15:49 -0600

    Write data out to files so we can load it back up on next start, normally I would not commit datafile but for this why not.

--- journal.py
+++ journal.py
@@ -1,8 +1,11 @@
 import datetime
+import json
 
 def main():
+    journal_file = './data/journal.json'
+
     banner()
-    run_event_loop()
+    run_event_loop(journal_file)
 
 
 def banner():
@@ -14,13 +17,14 @@ def menu():
     user_choice = input("Would you like to [A]dd, [L]ist journal entries or E[x]it: ").lower().strip()
     return user_choice
 
-def run_event_loop():
+def run_event_loop(journal_file):
 
     # set to False for init value of loop
     menu_entry = False
 
     # journal data, empty dict until we get json added.
-    journal_data = {}
+
+    journal_data = open_journal(journal_file)
 
     while menu_entry != 'x':
         menu_entry = menu()
@@ -28,6 +32,7 @@ def run_event_loop():
         if menu_entry == 'a':
             # Add a item
             journal_data = entry_add(journal_data)
+            write_journal(journal_data,journal_file)
         elif menu_entry == 'l':
             # list entries
             entry_list(journal_data)
@@ -50,6 +55,25 @@ def entry_list(data):
     for idx,key in enumerate(data):
         # add one to index for humans
         print(str(idx +1 ) + ": "  + key)
+
+def open_journal(jfile):
+    # open the file, create if not present
+    data = {}
+
+    try:
+        with open(jfile, 'r+') as input:
+            data = json.load(input)
+    except OSError:
+        print('Unable to open file {}'.format(jfile))
+
+    return data
+
+def write_journal(data,jfile):
+    try:
+        with open(jfile, 'w') as outfile:
+           json.dump(data, outfile, sort_keys=True, indent=4, default=str )
+    except OSError:
+        print('Unable to open file {}'.format(jfile))
 
 
 if __name__ == '__main__':
