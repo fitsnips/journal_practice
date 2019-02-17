47d7cdd8c6aaf34b51be19d313245aaadc207b0c
Joshua Miller <joshua.miller@rd.io>
2019-02-16 18:37:21 -0600

    Inital commit

---
+++ journal.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    banner()
+
+
+def banner():
+    print('-----------------------------------')
+    print('---       My Journal Practice   ---')
+    print('-----------------------------------')
+
+
+if __name__ == '__main__':
+    main()
